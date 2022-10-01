4be622abfb6f7bf00cd966563c2771995a6ac714
Eslam-Ayman8 <eslamayman13@hotmail.com>
2022-10-01 22:32:27 +0200

    String by Eslam

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct person{
+
+char *name;
+int age;
+char ID;
+
+}person;
+
+struct person_data{
+
+int CitNo;
+int salary;
+struct person per;
+
+}data;
+
+
+int main()
+{
+    person.name = "Eslam";
+    printf("The name: %s",person.name);
+
+
+    return 0;
+}
